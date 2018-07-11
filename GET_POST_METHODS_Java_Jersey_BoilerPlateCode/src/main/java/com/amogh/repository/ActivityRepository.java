f216a17b285efddb8bd0024359e02015e08e35d6
amoghcoolkarni983 <32130092+amoghcoolkarni983@users.noreply.github.com>
2018-07-11 10:38:34 +0530

    Add files via upload

---
+++ GET_POST_METHODS_Java_Jersey_BoilerPlateCode/src/main/java/com/amogh/repository/ActivityRepository.java
@@ -0,0 +1,15 @@
+package com.amogh.repository;
+
+import java.util.List;
+
+import com.amogh.activity.Activity;
+
+public interface ActivityRepository {
+
+	List<Activity> findAllActivities();
+
+	Activity findActivity(String activityId);
+
+	void create(Activity activity);
+
+}
